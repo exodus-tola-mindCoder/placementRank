e2297a96e16a7d2466fd3a49db690839b722cb9d
devtolii <tolagemechu4@gmail.com>
2025-01-09 20:32:43 -1200

    good night github

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
-import connectDB from './config/db';
+import connectDB from './config/db.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 5001;
 
 // create a route
+
+import authRoutes from './routes/authRoute.js';
 
 
 app.use(express.json());
