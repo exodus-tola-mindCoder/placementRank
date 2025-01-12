91ee1cf2439fc9be3589814d3165d6818a107d0e
devtolii <tolagemechu4@gmail.com>
2025-01-11 18:56:39 -1200

    signup updated

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -3,57 +3,68 @@ import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 
 export const signupController = async (req, res) => {
-    const { fullName, email, average, password } = req.body;
-    
-    try {
-        const userExists = await User.findOne({ email });
-        if (userExists) {
-            return res.status(400).json({ msg: "User already exists" });
-        };
 
+    try {
+        const { fullName, email, average, password } = req.body;
         if (fullName === "" || email === "" || average === "" || password === "") {
             return res.status(400).json({ msg: "Please fill in all fields" });
         };
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ success: false, message: "Invalid email" });
+        }
+        
         if (password.length < 6) {
             return res.status(400).json({ msg: "Password must be at least 6 characters" });
         };
+        const userExists = await User.findOne({ email });
 
-        const user = new User({
-            fullName,
-            email,
-            average,
-            password
+        if (userExists) {
+            return res.status(400).json({ msg: "User email already exists" });
+        };
+
+        const userExistByUsername = await User.findOne({
+            username: username
         });
+        if (userExistByUsername) {
+            return res.status(400).json({ msg: "Username already exists" });
+        };
 
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
         user.password = hashedPassword;// hash the password before saving it to the database
-        if (user) {
+        const newUser = new User({
+            fullName,
+            email,
+            average,
+            password: hashedPassword,
+        });
+        if (newUser) {
+            await newUser.save();
             res.status(201).json({
-                _id: user._id,
-                fullName: user.fullName,
-                email: user.email,
-                average: user.average,
-                password: user.password
-            })
-        }
+                success: true, User: {
+                ...newUser._doc,
+                    password: ""
+                },
+            });
+    };
 
         res.status(200).json({ msg: "User created successfully" });
 
-        await user.save();
 
     } catch (error) {
         console.log("Error", error.message);
         res.status(500).json({ msg: "Server Error" });
-        
+
     };
 };
 
 
 export const loginController = async (req, res) => {
-    const { email, password } = req.body;
-
+    
     try {
+        const { email, password } = req.body;
         const user = await User.findOne({ email });
         if (!user) {
             return res.status(400).json({ msg: "User does not exist" });
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -10,6 +10,12 @@ const studentSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+        
+    },
     password: {
         type: String,
         required: true
