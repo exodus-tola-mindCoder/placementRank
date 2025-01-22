3c726f00b4060a041cde1bc1a857df2c06504f2c
devtolii <tolagemechu4@gmail.com>
2025-01-22 05:38:28 -1200

    api calling

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import axios from 'axios';
 
-function api() {
-  return (
-    <div>api</div>
-  )
-}
+const API_URL = 'http://localhost:5001/api';
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export const register = async (studentData) => {
+  const response = await api.post('/auth/register', studentData);
+  return response.data;
+};
+
+export const login = async (studentId, email, password) => {
+  const response = await api.post('/auth/login', { studentId, email, password });
+  return response.data;
+};
+
+export const getStats = async () => {
+  const response = await api.get('/stats');
+  return response.data;
+};
+
+
+export const getDepartmentStats = async (department) => {
+  const response = await api.get(`/stats/department/${department}`);
+  return response.data;
+};
+
+export const updateDepartment = async (department, data) => {
+  const response = await api.put('/update-department', { department, data });
+  return response.data;
+};
+
+
+
+
+
+
+
+
+
+
 
 export default api
